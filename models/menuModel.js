2c1370768948f6ac851257b4094795e4683beb66
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-09-19 20:46:05 -0700

    manipulated and cached data

--- models/menuModel.js
+++ models/menuModel.js
@@ -7,8 +7,31 @@ async function add(item) {
 }
 
 // get all menu
-function getAll() {
-  return db("menu");
+// seperated them here in the backend so front-end can just get the data expected and
+// not having to filter all the data on the front.
+// I'm gonna cache this data because it wont change at all,
+async function getAll() {
+  const pizzas = await db("menu as m").where("m.category", "pizzas");
+  const salads = await db("menu as m").where("m.category", "salads");
+  const appetizers = await db("menu as m").where("m.category", "appetizers");
+  const pastas = await db("menu as m").where("m.category", "pastas");
+  const combos = await db("menu as m").where("m.category", "combos");
+  const sandwiches = await db("menu as m").where("m.category", "sandwiches");
+  const desserts = await db("menu as m").where("m.category", "desserts");
+  const drinks = await db("menu as m").where("m.category", "drinks");
+  const extras = await db("menu as m").where("m.category", "extras");
+
+  return {
+    pizzas: splitStr(pizzas),
+    salads: splitStr(salads),
+    appetizers: splitStr(appetizers),
+    pastas: splitStr(pastas),
+    combos: splitStr(combos),
+    sandwiches: splitStr(sandwiches),
+    desserts: splitStr(desserts),
+    drinks: splitStr(drinks),
+    extras: splitStr(extras),
+  };
 }
 
 function getBy(filter) {
@@ -28,6 +51,21 @@ function update(id, changes) {
 // remove item
 function remove(id) {
   return db("menu").where({ id }).del();
+}
+
+// split this properties so we can just iterate over them and add them as select inputs.
+function splitStr(arr) {
+  let updatedMenu = arr.map((menu) => {
+    return {
+      ...menu,
+      size_price: menu.size_price.split(","),
+      cheese: menu.cheese ? menu.cheese.split(",") : "",
+      dressing: menu.dressing ? menu.dressing.split(",") : "",
+      peppers: menu.peppers ? menu.peppers.split(",") : "",
+    };
+  });
+
+  return updatedMenu;
 }
 
 module.exports = {
